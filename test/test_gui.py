168e102bcf6078e9a2d7ac2b0923c4bb07e8a930
oldnick85 <tyapin_nikita@mail.ru>
2024-05-26 13:59:34 +0300

    [ref] file structure refactoring

--- test/test_gui.py
+++ test/test_gui.py
@@ -1,8 +1,17 @@
 from tkinter import Tk, Canvas
-import bi_tree
 import random
 import time
 from dataclasses import dataclass
+
+import os
+import sys
+project_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_directory)
+from src.bi_tree import Tree
+
+AREA_WIDTH = 600.0
+AREA_HEIGTH = 600.0
+POINTS_COUNT=1000
 
 @dataclass
 class GuiPoint:
@@ -17,42 +26,38 @@ class GuiPoint:
         if p.x < 0.0:
             p.vx = -p.vx
             p.x = 0.0
-        if p.x >= 600.0:
+        if p.x >= AREA_WIDTH:
             p.vx = -p.vx
-            p.x = 599.0
+            p.x = AREA_WIDTH-1
         p.y += p.vy
         if p.y < 0.0:
             p.vy = -p.vy
             p.y = 0.0
-        if p.y >= 600.0:
+        if p.y >= AREA_HEIGTH:
             p.vy = -p.vy
-            p.y = 599.0
+            p.y = AREA_HEIGTH-1
         return
 
 class GuiTk:
     def __init__(self) -> None:
         self.__root = Tk()
         self.__root.title("")
-        self.__root.geometry("600x600")
-        self.__canvas = Canvas(bg="white", width=600, height=600)
+        self.__root.geometry(f"{int(AREA_WIDTH)}x{int(AREA_HEIGTH)}")
+        self.__canvas = Canvas(bg="white", width=int(AREA_WIDTH), height=int(AREA_HEIGTH))
         self.__canvas.place(x=0, y=0)
         self.__canvas.bind('<Motion>', self.canvas_mouse_move)
         self.__canvas.bind("<Button-1>", self.canvas_mouse_click)
-        self.__graph = bi_tree.Tree(2, [0.0, 0.0], [600.0, 600.0], 2)
-        self.__graph.add_content([301.0, 301.0], "cursor")
-        self.__points = []
+        self.__graph = Tree(2, [0.0, 0.0], [AREA_WIDTH, AREA_HEIGTH], 2)
+        self.__graph.add_content([AREA_WIDTH/2, AREA_HEIGTH/2], "cursor")
+        self.__points : list[GuiPoint] = []
         self.__points_added = False
-        for _ in range(200):
-            self.__points.append(GuiPoint(x=random.uniform(0.0, 600.0), y=random.uniform(0.0, 600.0), vx=random.uniform(-1.0, 1.0), vy=random.uniform(-1.0, 1.0)))
-        self.__graph.add_content([50.0, 50.0], "A")
-        self.__graph.add_content([110.0, 110.0], "AA")
-        self.__graph.add_content([50.0, 100.0], "AAA")
-        self.__graph.add_content([70.0, 130.0], "AAAA")
-        self.__graph.add_content([100.0, 130.0], "AAAAA")
-        self.__graph.add_content([500.0, 500.0], "B")
-        self.__graph.add_content([500.0, 510.0], "BB")
+        for _ in range(POINTS_COUNT):
+            self.__points.append(GuiPoint(x=random.uniform(0.0, AREA_WIDTH), \
+                                          y=random.uniform(0.0, AREA_HEIGTH), \
+                                          vx=random.uniform(-1.0, 1.0), \
+                                          vy=random.uniform(-1.0, 1.0)))
         self.__in_radius : list[int] = []
-        self.__in_radius_center = [300.0, 300.0]
+        self.__in_radius_center = [AREA_WIDTH/2, AREA_HEIGTH/2]
         self.__in_radius_radius = 25
         return
 
@@ -67,7 +72,7 @@ class GuiTk:
         return
 
     def update(self) -> None:
-        if (not self.__points_added):
+        if not self.__points_added:
             for i, p in enumerate(self.__points):
                 self.__graph.add_content([p.x, p.y], i)
             self.__points_added = True
@@ -75,7 +80,7 @@ class GuiTk:
             for i, p in enumerate(self.__points):
                 p.velocity_step()
                 self.__graph.move_data(i, [p.x, p.y])
-            self.__in_radius = self.__graph.get_data_in_radius(self.__in_radius_center, self.__in_radius_radius)
+            self.__in_radius = self.__graph.get_data_in_radius( self.__in_radius_center, self.__in_radius_radius)
         return
 
     def draw(self) -> None:
@@ -84,7 +89,10 @@ class GuiTk:
         for area in areas:
             self.__canvas.create_rectangle(area.point_min()[0]+1, area.point_min()[1]+1, area.point_max()[0], area.point_max()[1], outline='grey')
 
-        self.__canvas.create_oval(self.__in_radius_center[0]-self.__in_radius_radius, self.__in_radius_center[1]-self.__in_radius_radius, self.__in_radius_center[0]+self.__in_radius_radius, self.__in_radius_center[1]+self.__in_radius_radius)
+        self.__canvas.create_oval(self.__in_radius_center[0]-self.__in_radius_radius, \
+                                  self.__in_radius_center[1]-self.__in_radius_radius, \
+                                  self.__in_radius_center[0]+self.__in_radius_radius, \
+                                  self.__in_radius_center[1]+self.__in_radius_radius)
 
         for i, p in enumerate(self.__points):
             c = "red" if (i in self.__in_radius) else "green"
